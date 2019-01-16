f97a7054a0dde5654d45aad4953a56f71bf51cba
Marck <marckcarrion@gmail.com>
2019-01-16 10:00:32 +0100

    Agregado todos los marker y una clase con los lugares

--- app/src/main/java/com/dev/marck/prom/mapboxtry/MainActivity.java
+++ app/src/main/java/com/dev/marck/prom/mapboxtry/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.dev.marck.prom.mapboxtry.MapResources.Places;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
@@ -29,20 +30,29 @@ public class MainActivity extends AppCompatActivity {
         mapView.onCreate(savedInstanceState);
 //        posicionamiento de camara
         CameraPosition init = new CameraPosition.Builder()
-                .target( new LatLng( 43.1410371,-2.9620557 ) )
-                .zoom( 16)
-                .tilt( 58.0 )
-                .bearing( 40 )
+                .target( Places.LLODIO)
+                .zoom( 14 )
+                .tilt( 0 )
+                .bearing( 0 )
                 .build();
-        MarkerOptions p = new MarkerOptions().position( new LatLng( 43.1410378,-2.9620560 ) );
+        MarkerOptions p = new MarkerOptions().position( Places.IKASTOLA );
+        MarkerOptions eliza = new MarkerOptions().position( Places.ELIZA );
         mapView.getMapAsync(
                 new OnMapReadyCallback() {
                     @Override
                     public void onMapReady( @NonNull MapboxMap mapboxMap ) {
 //                        Cargamos el estilo del mapa
                         mapboxMap.setStyle( new Style.Builder().fromUrl( getString( R.string.map_style_url ) ) );
-                        mapboxMap.animateCamera( CameraUpdateFactory.newCameraPosition(init), 300 );
                         mapboxMap.addMarker( p );
+                        mapboxMap.addMarker( eliza );
+                        mapboxMap.addMarker( new MarkerOptions().position( Places.TREN ) );
+                        mapboxMap.addMarker( new MarkerOptions().position( Places.ST_CRUZ ) );
+                        mapboxMap.addMarker( new MarkerOptions().position( Places.ZERAMIKA ) );
+                        mapboxMap.addMarker( new MarkerOptions().position( Places.JAUREGIA ) );
+                        mapboxMap.addMarker( new MarkerOptions().position( Places.DORRETXEA ) );
+                        mapboxMap.addMarker( new MarkerOptions().position( Places.PARKE ) );
+                        mapboxMap.animateCamera( CameraUpdateFactory.newCameraPosition(init), 300 );
+
                     }
                 }
         );
---
+++ app/src/main/java/com/dev/marck/prom/mapboxtry/MapResources/Places.java
@@ -0,0 +1,15 @@
+package com.dev.marck.prom.mapboxtry.MapResources;
+
+import com.mapbox.mapboxsdk.geometry.LatLng;
+
+public class Places {
+    public static final LatLng IKASTOLA = new LatLng( 43.1464882, -2.961911 );
+    public static final LatLng ELIZA = new LatLng( 43.1431389, -2.9623056 );
+    public static final LatLng LLODIO = new LatLng( 43.14103,-2.9645557 );
+    public static final LatLng TREN = new LatLng( 43.14263888, -2.96061111 );
+    public static final LatLng ST_CRUZ = new LatLng(43.133027778, -2.9699444444);
+    public static final LatLng ZERAMIKA = new LatLng(43.1335556, -2.969277778);
+    public static final LatLng JAUREGIA = new LatLng(43.1335, -2.970805555556);
+    public static final LatLng PARKE = new LatLng(43.1458611111, -2.9678611111);
+    public static final LatLng DORRETXEA = new LatLng(43.14722222, -2.969166667);
+}
