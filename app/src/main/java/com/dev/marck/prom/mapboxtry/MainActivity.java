0993f71fe338c54a0fb1971599a575448504b607
Marck <marckcarrion@gmail.com>
2019-01-18 10:28:12 +0100

    creado los eventos para los dos botones principales y creado el metodo para animar la camara

--- app/src/main/java/com/dev/marck/prom/mapboxtry/MainActivity.java
+++ app/src/main/java/com/dev/marck/prom/mapboxtry/MainActivity.java
@@ -1,8 +1,12 @@
 package com.dev.marck.prom.mapboxtry;
 
 import android.support.annotation.NonNull;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.dev.marck.prom.mapboxtry.MapResources.Places;
 import com.mapbox.mapboxsdk.Mapbox;
@@ -10,6 +14,7 @@ import com.mapbox.mapboxsdk.annotations.IconFactory;
 import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdate;
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
@@ -36,8 +41,8 @@ public class MainActivity extends AppCompatActivity {
                 .tilt( 0 )
                 .bearing( 0 )
                 .build();
-        MarkerOptions p = new MarkerOptions().position( Places.IKASTOLA ).setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_mk_red ));
-        MarkerOptions eliza = new MarkerOptions().position( Places.ELIZA ).setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_mk_red ));
+        MarkerOptions p = new MarkerOptions().position( Places.IKASTOLA ).setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_marker_x  ));
+        MarkerOptions eliza = new MarkerOptions().position( Places.ELIZA ).setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_marker_x  ));
         mapView.getMapAsync(
                 new OnMapReadyCallback() {
                     @Override
@@ -50,25 +55,57 @@ public class MainActivity extends AppCompatActivity {
                         mapboxMap.getUiSettings().setCompassEnabled( false );
                         mapboxMap.getUiSettings().setRotateGesturesEnabled( false );
                         mapboxMap.getGesturesManager().removeMoveGestureListener();
+                        mapboxMap.getUiSettings().setAttributionEnabled( false );
                         mapboxMap.addMarker( p );
                         mapboxMap.addMarker( eliza );
                         mapboxMap.addMarker( new MarkerOptions().position( Places.TREN )
-                            .setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_mk_green ) )).setTitle( "Tren Geltokia" );
+                            .setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_marker_x ) )).setTitle( "Tren Geltokia" );
                         mapboxMap.addMarker( new MarkerOptions().position( Places.ST_CRUZ )
-                                .setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_mk_green ))).setTitle( "Santa Cruz" );
+                                .setIcon( IconFactory.getInstance( MainActivity.this ).fromResource(  R.mipmap.ic_marker_x  ))).setTitle( "Santa Cruz" );
                         mapboxMap.addMarker( new MarkerOptions().position( Places.ZERAMIKA )
-                                .setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_mk_red )).setTitle( "Zeramika Lantegia" ) ).showInfoWindow( mapboxMap,mapView );
+                                .setIcon( IconFactory.getInstance( MainActivity.this ).fromResource(  R.mipmap.ic_marker_x  )));
                         mapboxMap.addMarker( new MarkerOptions().position( Places.JAUREGIA )
-                                .setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_mk_red )));
+                                .setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_marker_x  )));
                         mapboxMap.addMarker( new MarkerOptions().position( Places.DORRETXEA )
-                                .setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_mk_red )));
+                                .setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_marker_v  )));
                         mapboxMap.addMarker( new MarkerOptions().position( Places.PARKE )
-                                .setIcon( IconFactory.getInstance( MainActivity.this ).fromResource( R.mipmap.ic_mk_red )));
+                                .setIcon( IconFactory.getInstance( MainActivity.this ).fromResource(  R.mipmap.ic_marker_x )));
                         mapboxMap.animateCamera( CameraUpdateFactory.newCameraPosition(init), 300 );
 
                     }
                 }
         );
+
+        ConstraintLayout lay = findViewById( R.id.btn_jaraitu );
+        ConstraintLayout atera = findViewById( R.id.btn_atera );
+        atera.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                finish();
+            }
+        } );
+        lay.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick( View v ) {
+                lay.setVisibility( View.INVISIBLE );
+                atera.setVisibility( View.INVISIBLE );
+                viewPoint( Places.IKASTOLA );
+            }
+        } );
+    }
+
+//    quita los botones y la interfaz principal y hace zoom sobre un
+    private void viewPoint( LatLng pos ){
+        mapView.getMapAsync( new OnMapReadyCallback() {
+            @Override
+            public void onMapReady( @NonNull MapboxMap mapboxMap ) {
+                CameraPosition position = new CameraPosition.Builder()
+                        .target( pos )
+                        .zoom( 16 )
+                        .build();
+                mapboxMap.animateCamera( CameraUpdateFactory.newCameraPosition( position ),500 );
+            }
+        } );
     }
 
     @Override
